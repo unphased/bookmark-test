f39c27e2fac53861a9a0744ada6f7f0f21fc3476
Steven Lu <stevenlu443@gmail.com>
2014-09-17 02:28:43 -0400

    this addresses a couple of the requirements. Snapping behavior feels pretty good

--- bookmarkbar.js
+++ bookmarkbar.js
@@ -11,21 +11,28 @@ function $(){
   return document.querySelector.apply(document,arguments);
 }
 
+var snapthreshold = 15; // CSS pixels vertical snap threshold
+
 window.onload = function(){
   var dragging;
+  var dragitemwidth;
+  var unsnapped;
   var pos;
-  // I want to use jQuery! This is a really crappy substitute
+
+  // I want to use jQuery! This $ is a slovenly substitute
   $('ul').addEventListener("mousedown", function(event){
     dragging = event.target;
+    dragitemwidth = dragging.offsetWidth;
     pos = [event.clientX, event.clientY];
-    l('clicked ul on', event.target);
+    l('clicked ul on', event.target, 'it has width', dragitemwidth);
     return false;
   }, false);
 
   document.addEventListener("mousemove", function(event){
     if (!dragging) return;
     var offset = [event.clientX - pos[0], event.clientY - pos[1]];
-    dragging.style.transform = "translate3d("+offset[0]+"px,"+offset[1]+"px,0)";
+    unsnapped = Math.abs(offset[1]) > snapthreshold;
+    dragging.style.transform = "translate3d("+offset[0]+"px,"+(unsnapped?offset[1]:0)+"px,0)";
   });
 
   document.addEventListener("mouseup", function(event){
