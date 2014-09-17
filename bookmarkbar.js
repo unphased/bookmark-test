08e8cb68bec393cd44b25dec9d1d3408b6e841ad
Steven Lu <stevenlu443@gmail.com>
2014-09-17 02:40:23 -0400

    Writing a large comment for some code I am going to write next which turns out to be the first bit of code that is not self-explanatory

--- bookmarkbar.js
+++ bookmarkbar.js
@@ -25,6 +25,20 @@ window.onload = function(){
     dragitemwidth = dragging.offsetWidth;
     pos = [event.clientX, event.clientY];
     l('clicked ul on', event.target, 'it has width', dragitemwidth);
+    // Once an item is picked up, whether it is unsnapped or sliding around, it 
+    // may be important to note that
+    // 1. all items in the list can move because the dragging item can be slid 
+    //    to any position before or after all other items
+    // 2. there is a fixed range of motion for all of the other items. In fact, 
+    //    we find that we will be setting all of them to one of two possible 
+    //    positions, and these positions are determined by the size of the item 
+    //    being dragged and where it is currently being held.
+    // Specifically, for all items originally before the dragging item, their 
+    // possible positions are their current position (0) or dragitemwidth to the
+    // right (+diw) if dragging item is before it. For all items originally 
+    // after the dragging item, their possible positions are their current 
+    // position (0) or (-diw) if dragging item is after it. We can allow CSS 
+    // transitions to tween the transforms of all of the items efficiently.
     return false;
   }, false);
 
