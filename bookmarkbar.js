b0e3b1bfe8917b29a7d05cb34ec48fb49d50adac
Steven Lu <stevenlu443@gmail.com>
2014-09-17 01:52:08 -0400

    I took a bit of a break at the end of last commit. Here i am actually getting some dragging implemented

--- bookmarkbar.js
+++ bookmarkbar.js
@@ -2,25 +2,34 @@
 // sweet for purposes of demo/test
 
 // debug helper
-function l() {
+function l(){
   console.log.apply(console,arguments);
 }
 
 // replicate browsers' commandline API
-function $() {
+function $(){
   return document.querySelector.apply(document,arguments);
 }
 
-window.onload = function() {
-  var dragging = false;
+window.onload = function(){
+  var dragging;
+  var pos;
   // I want to use jQuery! This is a really crappy substitute
   $('ul').addEventListener("mousedown", function(event){
-    dragging = true;
+    dragging = event.target;
+    pos = [event.clientX, event.clientY];
     l('clicked ul on', event.target);
     return false;
   }, false);
-  $('ul').addEventListener("mouseup", function(){
+
+  document.addEventListener("mousemove", function(event){
+    if (!dragging) return;
+    var offset = [event.clientX - pos[0], event.clientY - pos[1]];
+    dragging.style.transform = "translate3d("+offset[0]+"px,"+offset[1]+"px,0)";
+  });
+
+  document.addEventListener("mouseup", function(event){
     dragging = false;
     return false;
   }, false);
-}
+};
