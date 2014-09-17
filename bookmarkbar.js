ae3b3e199e19673c10388fe32200a4344c973a05
Steven Lu <stevenlu443@gmail.com>
2014-09-17 15:36:36 -0400

    getting some stuff in place to calculate the position offsets for the remaining items in the list

--- bookmarkbar.js
+++ bookmarkbar.js
@@ -11,20 +11,46 @@ function $(){
   return document.querySelector.apply(document,arguments);
 }
 
+// helper for arrayifying stuff needed due to not having jQuery
+function toArray(obj) {
+  var array = [];
+  // iterate backwards ensuring that length is positive integer
+  for (var i = Math.max(obj.length >> 0, 0); i--;) { 
+    array[i] = obj[i];
+  }
+  return array;
+}
+
+// not very generally useful helper due to no jQuery
+function parentIsTag(domelem, tag) {
+  do {
+    if (!domelem || domelem.tagName == tag)
+      return domelem;
+  } while(domelem = domelem.parentNode);
+}
+
 var snapthreshold = 15; // CSS pixels vertical snap threshold
 
 window.onload = function(){
   var dragging;
   var dragitemwidth;
+  var dragitemstartx;
   var unsnapped;
   var pos;
+  var itemlocationdata;
 
-  // I want to use jQuery! This $ is a slovenly substitute
+  // I want to use jQuery -- so far I would have used it to double up on the 
+  // evenet handlers. For example, when handling only single touch and mouse you
+  // can get away with just assigning the same event handlers to mousedown and 
+  // touchstart
   $('ul').addEventListener("mousedown", function(event){
-    dragging = event.target;
+    dragging = parentIsTag(event.target, "LI");
     dragitemwidth = dragging.offsetWidth;
     pos = [event.clientX, event.clientY];
-    l('clicked ul on', event.target, 'it has width', dragitemwidth);
+    dragitemstartx = dragging.getBoundingClientRect().left -
+                     document.body.getBoundingClientRect().left;
+                     // grabbing the page position
+    l('clicked ul on', event.target, 'it has width', dragitemwidth, 'positioned with left side at', dragitemstartx);
     // Once an item is picked up, whether it is unsnapped or sliding around, it 
     // may be important to note that
     // 1. all items in the list can move because the dragging item can be slid 
@@ -42,6 +68,19 @@ window.onload = function(){
     // Note also that the offset values are just diw because no margin exists on
     // the items. Ordinarily the offset is width+(marginleft+marginright)/2 or 
     // some such.
+
+    // cache the layout of the list in a variable once per mouse interaction 
+    // (rather than accessing the DOM in a loop once per mouse movement!)
+    itemlocationdata = toArray($('ul').children).map(function(x){
+      if (x == dragging) return "dragged";
+      // do note that doc.body.gBCR cannot be cached, suppose the zoom of the 
+      // page changes or the page is scrolled while zoomed in (Safari!), all the
+      // BCR coordinates change. This computed difference, however, conveniently 
+      // remains constant in CSS-pixels.
+      var thisitempos = x.getBoundingClientRect().left - document.body.getBoundingClientRect().left;
+      return {el: x, wasAfter: thisitempos > dragitemstartx, pos: thisitempos};
+    });
+    l('itemlocationdata', itemlocationdata);
     return false;
   }, false);
 
@@ -49,11 +88,24 @@ window.onload = function(){
     if (!dragging) return;
     var offset = [event.clientX - pos[0], event.clientY - pos[1]];
     unsnapped = Math.abs(offset[1]) > snapthreshold;
-    dragging.style.transform = "translate3d("+offset[0]+"px,"+(unsnapped?offset[1]:0)+"px,0)";
+    // correct the positioning of the remaining items by using transform
+    dragging.style.transform = dragging.style['-webkit-transform'] =
+      "translate3d("+offset[0]+"px,"+(unsnapped?offset[1]:0)+"px,0)";
+    if (!unsnapped) { l('x', offset[0]); }
+    // this can be optimized! (loops thru the cached item list each mousemove)
+    // we can optimize this by only searching the range that the last two mouse 
+    // positions have traversed. However, the additional complexity will cause 
+    // it to be slower overall when there is a small number of items in the 
+    // bookmarklist.
+    for (var i=itemlocationdata.length; i--;) {
+      // if the right side of dragged is past the middle, send it to minus
+    }
   });
 
   document.addEventListener("mouseup", function(event){
     dragging = false;
+    // apply the positioning of all items by actually moving them and zeroing 
+    // their transform styles
     return false;
   }, false);
 };
