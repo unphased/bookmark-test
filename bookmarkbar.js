31337e46c962580d1e0d1ca84b1a50771a664f1a
Steven Lu <stevenlu443@gmail.com>
2014-09-17 01:22:20 -0400

    got some events working

--- bookmarkbar.js
+++ bookmarkbar.js
@@ -1,1 +1,26 @@
+// skipping as much boilerplate stuff as I can to keep everything short and 
+// sweet for purposes of demo/test
 
+// debug helper
+function l() {
+  console.log.apply(console,arguments);
+}
+
+// replicate browsers' commandline API
+function $() {
+  return document.querySelector.apply(document,arguments);
+}
+
+window.onload = function() {
+  var dragging = false;
+  // I want to use jQuery! This is a really crappy substitute
+  $('ul').addEventListener("mousedown", function(event){
+    dragging = true;
+    l('clicked ul on', event.target);
+    return false;
+  }, false);
+  $('ul').addEventListener("mouseup", function(){
+    dragging = false;
+    return false;
+  }, false);
+}
